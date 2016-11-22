01874db4dc048eab02e9be904e83383fc486347d
juanmix <juancschap@gmail.com>
2016-11-22 01:31:08 +0100

    4.4 created order model, forgot to add product_id field and messed up

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,9 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-  def landing_page
-    @featured_product = Product.first
-  end
+  #def landing_page
+    #@featured_product = Product.first
+  #end
 
 end
 
