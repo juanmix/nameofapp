ced7020e60f2b6961bd648253d12ab66bdade331
juanmix <juancschap@gmail.com>
2016-11-24 14:48:06 +0100

    4.5 added carousel, created _product partial

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,10 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-  #def landing_page
+  def landing_page
     #@featured_product = Product.first
-  #end
+    @products = Product.limit(3)
+  end
 
 end
 
