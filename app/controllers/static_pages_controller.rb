b4a8035644ceaf5250cc4ba295c32db0e7b88d0a
juanmix <juancschap@gmail.com>
2016-12-12 00:47:02 +0100

    5.4 styling contact_form.html.erb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -16,11 +16,12 @@ class StaticPagesController < ApplicationController
     #we call the deliver_now method from inside our controller to send the email to user
     UserMailer.contact_form(@email, @name, @message).deliver_now
 
-    # message sent to admin??
-    ActionMailer::Base.mail(:from => @email,
-        :to => 'juancschap@gmail.com',
-        :subject => "A new contact form message from #{@name}",
-        :body => @message).deliver_now
+    # '.mail' method for message sent to admin thru contact form page. not necessary here, already done in...
+    #...user_mailer.rb , they do the same thing.
+    #ActionMailer::Base.mail(:from => @email,
+        #:to => 'juancschap@gmail.com',
+        #:subject => "A new contact form message from #{@name}",
+        #:body => @message).deliver_now
   end
 
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,13 @@
 class UserMailer < ApplicationMailer
   default from: "from@example.com"
 
+  # contact_form Method. for emails sent by user to admin thru contact form page.
   def contact_form(email, name, message)  # mailer class wont have access to vars in our...
     #... controller action, so we pass them into mailer method as params.
   @message = message
-    mail(:from => email,
-      :to => 'your-email@example.com',
-      :subject => "A new contact form message from #{name}")
+    mail(from: email,
+      to: 'juancschap@gmail.com',
+      subject: "A new contact form message from #{name}")
   end
 
 end
