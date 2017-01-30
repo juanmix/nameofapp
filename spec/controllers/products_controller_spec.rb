116fece4692980b1dd17edfaf4886f713aadb841
juanmix <juancschap@gmail.com>
2017-01-30 15:36:21 +0100

    6.15 products controller tests

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -15,4 +15,25 @@ describe ProductsController, type: :controller do
       expect(response).to render_template('index')
     end
   end
+
+  context 'POST #create' do
+    before do
+      @product = FactoryGirl.create(:product)
+    end
+
+    it 'redirects to product show' do
+      expect(response).to redirect_to(product_path)
+    end
+  end
+
+  context 'PATCH #update' do
+    before do
+      @product = FactoryGirl.create(:product)
+    end
+
+    it 'redirects to product show' do
+      expect(response).to redirect_to(product_path)
+    end
+  end
+
 end
--- spec/factories/comment_factory.rb
+++ spec/factories/comment_factory.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :comment do
     rating 1
+    product
     user
     body "bla bla bla bike!"
   end
